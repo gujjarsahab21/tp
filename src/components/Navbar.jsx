2be5bdfe36c7ed0367b3a5481347c9f3effa2bd7
Harsh Gurjar <harshgujjar2120@gmail.com>
2024-05-27 13:45:21 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+    return (
+      <div className="my-4 flex h-[60px] items-center justify-center gap-2 rounded-lg bg-white text-xl font-medium">
+        <img src="https://raw.githubusercontent.com/anshuopinion/React-10-Projects/001aff889270e5554dd20c7d23ca5aa84e69ab55/project-5/public/firebase.svg" />
+        <h1>Firebase Contact App</h1>
+      </div>
+    );
+  };
+  
+  export default Navbar;
